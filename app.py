fd09585512089e1ad609e973959b57761912f5db
c19worldnews <97426345+c19worldnews@users.noreply.github.com>
2022-09-04 16:57:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ def prediction(city_name,country_name,iso_code,label_alpha_2,select_location):
     elements=wd.find_elements_by_xpath("//button[contains(@class,'showall')]")
      for element in elements:
       wd.execute_script("arguments[0].click();", element);
-     return element.text
+      return element.text
     # Get 'HTML'
 
     html_data = wd.page_source
