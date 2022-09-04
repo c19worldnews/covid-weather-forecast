ae965dbb4706064bb07c0a6c6849f29ebecdf1db
c19worldnews <97426345+c19worldnews@users.noreply.github.com>
2022-09-04 17:03:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ def prediction(city_name,country_name,iso_code,label_alpha_2,select_location):
  # element = wd.find_element(By.XPATH,"//*[@id='world-france-covid-cases']/div/div/main/div[3]/div[4]/section[2]/div[2]/table/tbody/tr")
   #wd.execute_script("arguments[0].click();", element)
   #print(select_location)
-  if select_location != 'France' OR select_location != 'Australia' :
+  if select_location != 'France' or select_location != 'Australia' :
     elements=wd.find_elements_by_xpath("//button[contains(@class,'showall')]")
     for element in elements:
       wd.execute_script("arguments[0].click();", element);
